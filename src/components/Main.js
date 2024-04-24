8a42d85ea903d1f069919b0765f3339dc2296b4d
hyunsangwon <hyunsangwon93@gmail.com>
2024-04-24 18:43:00 +0900

    oss

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <div className="article">Article</div>
+        <div className="nav">Nav</div>
+        <div className="aside">Aside</div>
+    </div>
+  )
+}
+
+export default Main
