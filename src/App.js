2d6ad12e3d9c3bfe30daea1cf9326df30b7f9422
hyunsangwon <hyunsangwon93@gmail.com>
2024-04-25 14:47:11 +0900

    ㅇㅇ

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Main from './components/Main';
 function App() {
   return (
     <div>
-      {/* export한 컴포넌트를 불러온다. */}
+      {/* 
+      html을 리턴함 이런 문법을 JSX(JavaScript Syntax eXtension) 라고 함
+      export한 컴포넌트를 불러온다. */}
       <Header></Header>
       <Main></Main>
       <Footer></Footer>
