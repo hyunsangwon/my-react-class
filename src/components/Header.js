dfa1b60bbee015589bc29d3264f2a26be6f48480
hyunsangwon <hyunsangwon93@gmail.com>
2024-04-25 15:07:29 +0900

    React 1일차 내용정리

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//에로우 함수로 컴포넌트 만들기
 const Header = () => {
   return <div className="header">Header</div>;
 };
--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//ReactDOM(Document Object Model)은 VirtualDOM이라고 함.
+//기존 페이지는 변경된 데이터가 있을 때 화면 전체를 랜더링 함.
+//렌더링이란 -> 브라우저에 우리가 만든 html,css,js를 보여주는 작업을 의미
+//페이지에 데이터가 많아질수록 기존 페이지는 느려질 수 밖에 없음.  -> 리엑트 가상돔은 이 문제를 변경된 부분만 렌더링 함.
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//StrictMode은 개발자가 빠른 에러를 해결할 수 있도록 검사와 경고메세지를 전달합니다.
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
