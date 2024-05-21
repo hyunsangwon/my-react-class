749810cb7ef2b67f69de9dfd7727dd3d20804b69
hyunsangwon <hyunsangwon93@gmail.com>
2024-05-21 14:02:27 +0900

    oss

---
+++ src/components_0523/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Main = () => {
+  const USER = [{ name: 'King' }, { name: 'Lee' }, { name: 'Jones' }, { name: 'Smith' }, { name: 'James' }, { name: 'Ray' }];
+  return <div>Main</div>;
+};
+
+export default Main;
